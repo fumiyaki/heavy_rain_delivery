d187cb0b9948f97bc7fc8e0508d40cf4eec7976d
fumiya yono <z2ab8q3ty0@gmail.com>
2021-09-25 06:57:56 +0900

    Map.tsxでMapを表示できるように修正

--- src/organisms/Map.tsx
+++ src/organisms/Map.tsx
@@ -1,52 +1,52 @@
+import mapData from "../data/mapData";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import mapData from "../data/tokyo23.json";
-import highchartsMap from "highcharts/modules/map";
-highchartsMap(Highcharts);
+import { useEffect } from "react";
 
-const setLocation = (position: GeolocationPosition) => {
-  // 緯度・経度を取得
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
+const App = () => {
+  useEffect(() => {
+    // geolocation に対応しているか否かを確認
+    if ("geolocation" in navigator) {
+      var opt = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      };
+      navigator.geolocation.getCurrentPosition(setLocation, showErr, opt);
+    } else {
+      alert("ブラウザが位置情報取得に対応していません");
+    }
+  }, []);
 
-  // 緯度・経度を表示
-  console.log(lat, lon);
-};
+  const setLocation = (position: GeolocationPosition) => {
+    // 緯度・経度を取得
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-// エラー時に呼び出される関数
-const showErr = (positionError: GeolocationPositionError) => {
-  if (positionError.code === 1) {
-    alert("位置情報の利用が許可されていません");
-  } else if (positionError.code === 2) {
-    alert("デバイスの位置が判定できません");
-  } else if (positionError.code === 3) {
-    alert("タイムアウトしました");
-  } else {
-    alert(positionError.message);
-  }
-};
-
-// geolocation に対応しているか否かを確認
-if ("geolocation" in navigator) {
-  var opt = {
-    enableHighAccuracy: true,
-    timeout: 10000,
-    maximumAge: 0,
+    // 緯度・経度を表示
+    console.log(lat, lon);
   };
-  navigator.geolocation.getCurrentPosition(setLocation, showErr, opt);
-} else {
-  alert("ブラウザが位置情報取得に対応していません");
-}
 
-const Map = () => {
-  const mapOptions: Highcharts.Options = {
+  // エラー時に呼び出される関数
+  const showErr = (positionError: GeolocationPositionError) => {
+    if (positionError.code === 1) {
+      alert("位置情報の利用が許可されていません");
+    } else if (positionError.code === 2) {
+      alert("デバイスの位置が判定できません");
+    } else if (positionError.code === 3) {
+      alert("タイムアウトしました");
+    } else {
+      alert(positionError.message);
+    }
+  };
+
+  const mapOptions = {
     title: {
-      text: "ここここおここここｋ",
+      text: "heavy rain map",
     },
     mapNavigation: {
       enabled: true,
     },
-
     responsive: {
       rules: [
         {
@@ -74,9 +74,8 @@ const Map = () => {
     },
     series: [
       {
-        type: "heatmap",
         animation: {
-          defer: 1000,
+          duration: 1000,
         },
         dataLabels: {
           enabled: true,
@@ -89,26 +88,19 @@ const Map = () => {
           ["13105", 10],
         ],
       },
-      {
-        type: "mapline",
-        name: "Separators",
-        data: Highcharts.geojson(mapData, "mapline"),
-        nullColor: "gray",
-        showInLegend: false,
-        enableMouseTracking: false,
-      },
     ],
   };
 
   return (
     <div>
+      <h1>Demos</h1>
       <HighchartsReact
-        options={mapOptions}
-        constructorType={"mapChart"}
         highcharts={Highcharts}
+        constructorType={"mapChart"}
+        options={mapOptions}
       />
     </div>
   );
 };
 
-export default Map;
+export default App;
