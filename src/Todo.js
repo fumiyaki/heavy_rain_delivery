7519fe1fed94b91f5ead436ad93a39026ee11c47
Keito <54165015+Tanesan@users.noreply.github.com>
2021-09-24 16:58:15 +0900

    [add] 現在地取得機能

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,39 @@ import HighchartsReact from 'highcharts-react-official'
 import mapData from "./data/tokyo23.json";
 import highchartsMap from "highcharts/modules/map";
 highchartsMap(Highcharts);
+
+function setLocation(pos){
+
+	// 緯度・経度を取得
+	let lat = pos.coords.latitude;
+	let lon = pos.coords.longitude;
+
+	// 緯度・経度を表示
+	console.log(lat,lon);
+}
+
+// エラー時に呼び出される関数
+function showErr(err){
+	switch(err.code){
+		case 1 : alert("位置情報の利用が許可されていません"); break;
+		case 2 : alert("デバイスの位置が判定できません"); break;
+		case 3 : alert("タイムアウトしました"); break;
+		default : alert(err.message);
+	}
+}
+
+// geolocation に対応しているか否かを確認
+if("geolocation" in navigator){
+	var opt = {
+		"enableHighAccuracy": true,
+		"timeout": 10000,
+		"maximumAge": 0,
+	};
+	navigator.geolocation.getCurrentPosition(setLocation, showErr, opt);
+}else{
+	alert("ブラウザが位置情報取得に対応していません");
+}
+
 export default function Todo() {
   const mapOptions = {
     title: {
