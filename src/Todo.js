8c6d9daa65735e98dc2012031b6eeba09ca76a83
Keito <54165015+Tanesan@users.noreply.github.com>
2021-09-24 13:33:56 +0900

    add map

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,38 @@
-import React from "react";
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import mapData from "./data/alljapan.json";
+import highchartsMap from "highcharts/modules/map";
+highchartsMap(Highcharts);
+export default function Todo() {
+  const mapOptions = {
+    title: {
+      text: "d",
+    },
+    colorAxis: {
+      min: 0,
+      stops: [
+        [0, "#EFEFFF"],
+        [0.67, "#4444FF"],
+        [1, "#000022"],
+      ],
+    },
+    series: [
+      {
+        mapData: mapData,
+        name: "Norway",
+        data: [],
+      },
+    ],
+  };
 
-const Todo = () => {
-  return <div>aaa</div>;
-};
-
-export default Todo;
+  return (
+    <div>
+      <h2>Highcharts</h2>
+      <HighchartsReact
+        options={mapOptions}
+        constructorType = { 'mapChart' }
+        highcharts={Highcharts}
+      />
+    </div>
+  );
+}
