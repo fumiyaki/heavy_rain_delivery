33f5f8139a9530a949ab5ada4ba9e2c55104498a
fumiya yono <z2ab8q3ty0@gmail.com>
2021-09-26 12:38:15 +0900

    speedDialを地図上に表示

--- src/molecules/SpeedDial.tsx
+++ src/molecules/SpeedDial.tsx
@@ -16,21 +16,19 @@ const actions = [
 
 const SpeedDial = () => {
   return (
-    <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
-      <MaterialUISpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: "absolute", bottom: 16, right: 16 }}
-        icon={<SpeedDialIcon />}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-          />
-        ))}
-      </MaterialUISpeedDial>
-    </Box>
+    <MaterialUISpeedDial
+      ariaLabel="SpeedDial basic example"
+      sx={{ position: "absolute", bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon />}
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+        />
+      ))}
+    </MaterialUISpeedDial>
   );
 };
 
